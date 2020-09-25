f8c246e29235687376234811aad9ead22ec377a6
YEONGJAE LEE <lyj8520675@naver.com>
2020-09-25 20:17:34 +0900

    Add files via upload

---
+++ fast_combination.cpp
@@ -0,0 +1,36 @@
+ll fac[N];
+unsigned long long power(unsigned long long x, int y, int p)
+{
+    unsigned long long res = 1;
+    x = x % p;
+
+    while (y > 0) {
+        // If y is odd, multiply x with result 
+        if (y & 1)
+            res = (res * x) % p;
+
+        // y must be even now 
+        y = y >> 1; // y = y/2 
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+unsigned long long modInverse(unsigned long long n, int p)
+{
+    return power(n, p - 2, p);
+}
+
+unsigned long long nCrModPFermat(unsigned long long n,
+    int r, int p)
+{
+    // Base case 
+    if (r == 0)
+        return 1;
+
+    // Fill factorial array so that we 
+    // can find all factorial of r, n 
+    // and n-r 
+
+    return (fac[n] * modInverse(fac[r], p) % p * modInverse(fac[n - r], p) % p) % p;
+}
